1a128bbd57b4e5bb7789c2b8a3c59d5bea45daba
lucas-dantas-acelera-maker <aluno17@aceleramaker.com.br>
2025-04-16 16:13:19 -0300

    feat: add address pipe in user details address

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { PhonePipe } from './phone/phone.pipe';
-import { AddressPipe } from './address.pipe';
+import { AddressPipe } from './address/address.pipe';
 
 @NgModule({
   declarations: [PhonePipe, AddressPipe],
-  exports: [PhonePipe],
+  exports: [PhonePipe, AddressPipe],
 })
 export class PipesModule {}
