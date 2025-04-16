4688bfbae62a272409d3e0077065476004a741f0
lucas-dantas-acelera-maker <aluno17@aceleramaker.com.br>
2025-04-16 16:20:14 -0300

    feat: add status pipe in user details status

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PhonePipe } from './phone/phone.pipe';
 import { AddressPipe } from './address/address.pipe';
+import { StatusPipe } from './status/status.pipe';
 
 @NgModule({
-  declarations: [PhonePipe, AddressPipe],
-  exports: [PhonePipe, AddressPipe],
+  declarations: [PhonePipe, AddressPipe, StatusPipe],
+  exports: [PhonePipe, AddressPipe, StatusPipe],
 })
 export class PipesModule {}
