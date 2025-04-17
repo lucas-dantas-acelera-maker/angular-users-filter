288a0306c55c96466fefd56c6b709ee8ea8ecefb
lucas-dantas-acelera-maker <aluno17@aceleramaker.com.br>
2025-04-17 12:11:02 -0300

    feate: simulating HTTP request with usersList input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UsersList } from './data/users-list';
 import { IUser } from './interfaces/user/user.interface';
 
@@ -7,9 +7,16 @@ import { IUser } from './interfaces/user/user.interface';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   userSelected: IUser = {} as IUser;
   showUserDetails: boolean = false;
+  users: IUser[] = [];
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.users = UsersList;
+    }, 2500);
+  }
 
   onUserSelected(user: IUser) {
     this.userSelected = user;
--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,5 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { UsersList } from 'src/app/data/users-list';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IUser } from 'src/app/interfaces/user/user.interface';
 
 @Component({
@@ -8,8 +7,9 @@ import { IUser } from 'src/app/interfaces/user/user.interface';
   styleUrls: ['./users-list.component.scss'],
 })
 export class UsersListComponent {
-  usersList: IUser[] = UsersList;
   displayedColumns: string[] = ['name', 'date', 'status'];
+
+  @Input({ required: true }) usersList: IUser[] = [];
 
   @Output('userSelected')
   userSelectedEmit = new EventEmitter<IUser>();
